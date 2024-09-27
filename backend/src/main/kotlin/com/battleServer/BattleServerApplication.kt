5e053a5aba7edfe2c0f9ac58f451c23d0815b482
wesda994 <danielwest618@gmail.com>
2024-09-27 23:45:07 +1200

    Created a CreateGame method

--- backend/src/main/kotlin/com/battleServer/BattleServerApplication.kt
+++ backend/src/main/kotlin/com/battleServer/BattleServerApplication.kt
@@ -1,18 +1,35 @@
 package com.battleServer
 
+import com.battleServer.domains.Game
+import jakarta.servlet.http.HttpServletRequest
 import org.springframework.boot.runApplication
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @SpringBootApplication
 class BattleServerApplication {
 	@GetMapping("")
-	fun helloWorld() : String  = "Hello World"
+	fun helloWorld(): String = "Hello World"
 
+	@PostMapping("/createGame")
+	fun createGame(request: HttpServletRequest): Game {
+		return Game(getRandomString(16), listOf(1), listOf(1), "SetUp", request.remoteAddr, null, null, null)
+
+	}
 }
+fun getRandomString(length: Int) : String {
+	val allowedChars = ('A'..'Z') + ('a'..'z') + ('0'..'9')
+	return (1..length)
+		.map { allowedChars.random() }
+		.joinToString("")
+}
+
 
 fun main(args: Array<String>) {
 	runApplication<BattleServerApplication>(*args)
--- backend/src/main/kotlin/com/battleServer/domains/Game.kt
+++ backend/src/main/kotlin/com/battleServer/domains/Game.kt
@@ -4,8 +4,9 @@ data class Game(
     val lobbyCode:String,
     val hostBoard:List<Int>,
     val guestBoard:List<Int>,
-    val currentTurn:String,
+    val currentTurn:String?,
     val hostIP:String,
-    val guestIP:String,
-    val host:Player,
-    val guest:Player)
+    val guestIP:String?,
+    val host:Player?,
+    val guest:Player?
+)
