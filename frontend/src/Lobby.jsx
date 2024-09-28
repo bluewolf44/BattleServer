2cae792387d88b3c748c1bbaed27c03ca97cdca0
Veronika Luthar <veronika.luthar@gmail.com>
2024-09-28 13:21:14 +1200

    Move accounts buttons to bottom right corner of screen

--- frontend/src/Lobby.jsx
+++ frontend/src/Lobby.jsx
@@ -4,10 +4,6 @@ function Lobby({HandleHost}) {
 
     return (
         <>
-            <div>
-                <button className = "AccountButton">Sign up</button>
-                <button className = "AccountButton">Log in</button>
-            </div>
             <h1>Welcome to BattleSever</h1>
             <div id = "ButtonsDiv">
                 <div id = "JoinGameDiv">                    
@@ -16,6 +12,10 @@ function Lobby({HandleHost}) {
                 </div>
                 <p>or</p>
                 <button onClick={HandleHost}>Host Game</button>
+            </div>
+            <div id = "AccountsDiv">
+                <button className = "AccountButton">Sign up</button>
+                <button className = "AccountButton">Log in</button>
             </div>
         </>
     )
