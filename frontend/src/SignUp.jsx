aa7c7543a64841422ea7203ea679fabdbe854351
wesda994 <danielwest618@gmail.com>
2024-10-06 11:05:02 +1300

    Only on creation

--- frontend/src/SignUp.jsx
+++ frontend/src/SignUp.jsx
@@ -43,7 +43,7 @@ export default function SignUp({setError}){
             {
                 setError("Username already exists");
             }
-            else
+            else if (response.status == 201)
             {
                 setError("Account created");
             }
