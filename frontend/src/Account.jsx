42423a4d924ebefd2c65e6fd6aabcd07c020aa7d
wesda994 <danielwest618@gmail.com>
2024-10-07 14:58:15 +1300

    Try

--- frontend/src/Account.jsx
+++ frontend/src/Account.jsx
@@ -83,7 +83,7 @@ export default function Account({setError,idToLambda,user,setUser}){
                     setError("Sign In success");
                 }
                 console.log(res);
-                return res.json().body.json();
+                return res.json().body;
             }).then((data) => {
                 console.log(data)
                 setUser(data);
